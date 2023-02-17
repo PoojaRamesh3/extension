43a7edfa2a788859f660189ba129bca03569a5d3
Pooja R <pooja.ramesh@imaginelearning.com>
2023-02-17 20:31:50 +0530

    changes

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,46 +1,51 @@
 import React, { useState } from 'react';
 import TodoForm from './TodoForm';
-import { RiCloseCircleLine } from 'react-icons/ri';
-import { TiEdit } from 'react-icons/ti';
+import { MdClose } from 'react-icons/md';
+import { AiFillEdit } from 'react-icons/ai';
+import { FiCheck } from 'react-icons/fi';
 
 const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
-  const [edit, setEdit] = useState({
-    id: null,
-    value: ''
-  });
-
-  const submitUpdate = value => {
-    updateTodo(edit.id, value);
-    setEdit({
-      id: null,
-      value: ''
+    const [edit, setEdit] = useState({
+        id: null,
+        value: '',
     });
-  };
 
-  if (edit.id) {
-    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
-  }
+    const submitUpdate = (value) => {
+        updateTodo(edit.id, value);
+        setEdit({
+            id: null,
+            value: '',
+        });
+    };
 
-  return todos.map((todo, index) => (
-    <div
-      className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
-      key={index}
-    >
-      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-        {todo.text}
-      </div>
-      <div className='icons'>
-        <RiCloseCircleLine
-          onClick={() => removeTodo(todo.id)}
-          className='delete-icon'
-        />
-        <TiEdit
-          onClick={() => setEdit({ id: todo.id, value: todo.text })}
-          className='edit-icon'
-        />
-      </div>
-    </div>
-  ));
+    if (edit.id) {
+        return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+    }
+
+    return todos.map((todo, index) => (
+        <div
+            className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+            key={index}
+        >
+            <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+                {todo.text}
+            </div>
+            <div className="icons">
+                <FiCheck
+                    onClick={() => completeTodo(todo.id)}
+                    className="check-icon"
+                />
+                <MdClose
+                    onClick={() => removeTodo(todo.id)}
+                    className="delete-icon"
+                />
+                <AiFillEdit
+                    onClick={() => setEdit({ id: todo.id, value: todo.text })}
+                    className="edit-icon"
+                />
+            </div>
+        </div>
+    ));
 };
 
 export default Todo;
